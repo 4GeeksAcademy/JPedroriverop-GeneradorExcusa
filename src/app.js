84c8d9f28fed367cec03c2a01bdfbb20c5b77cea
J Pedro Rivero P <129573462+PedroRiverop@users.noreply.github.com>
2024-08-31 02:50:28 +0000

    Completo, verificar tamaño img de bird

--- src/app.js
+++ src/app.js
@@ -7,5 +7,49 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let iwho = Math.floor(Math.random() * who.length);
+
+  let excusa =
+    who[iwho] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+
+  console.log(excusa);
+
+  document.getElementById("excuse").innerHTML = excusa;
+
+  let imgPath = "src/assets/img/rigo-baby.jpg";
+  switch (iwho) {
+    case 0:
+      imgPath = "src/assets/img/dog.jpg";
+      break;
+    case 1:
+      imgPath = "src/assets/img/grandma.png";
+      break;
+    case 2:
+      imgPath = "src/assets/img/mailman.jpg";
+      break;
+    case 3:
+      imgPath = "src/assets/img/bird.jpg";
+      break;
+  }
+
+  const myImageContainer = document.getElementById("imagen");
+
+  myImageContainer.innerHTML = `<img src="${imgPath}" alt="Imagen">`;
 };
